59d8a34120a1665aae8936ba9e5e2c8e4c7f4308
Stefan Eng <stefaneng13@gmail.com>
2016-04-18 20:50:00 -0700

    stopwords and cleaup in python script

--- scripts/split_file.py
+++ scripts/split_file.py
@@ -5,6 +5,13 @@ import re
 def clean_article_name(name):
     return name.replace('/', '_')
 
+def clean_article(article):
+    article = article.lower()
+    article = re.sub('\(.*\)', '', article)
+    article = re.sub('\[.*\]', '', article)
+    article = re.sub('[^a-zA-Z\s:]', '', article)
+    return article
+
 def split_file(f_name, dir='.'):
     curr_article = []
     article_name = None
@@ -34,11 +41,12 @@ def oneline_file(f_name, output_file):
             s_line = line.strip()
             m = re.search('<title>(.*)</title>', s_line)
             if m and article_name:
-                # article_name: full text contents dict
-                article = ' '.join(curr_article)
-                f.write(article)
-                f.write('\n')
-                article_name = clean_article_name(m.group(1))
+                article_name = m.group(1)
+                if not article_name.startswith('Talk:'):
+                    article = ' '.join(curr_article)
+                    f.write(clean_article(article))
+                    f.write('\n')
+                    article_name = clean_article_name(m.group(1))
                 curr_article = []
             elif m:
                 article_name = clean_article_name(m.group(1))
